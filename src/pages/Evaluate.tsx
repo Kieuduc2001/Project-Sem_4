305bd6e7ebd9d477f2a11f369ec9e329bed1f7ec
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2024-07-03 15:14:41 +0700

    pull code from branch dev (#39)

--- src/pages/Evaluate.tsx
+++ src/pages/Evaluate.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { DatePicker, Form, Modal, Space } from 'antd';
 import dayjs from 'dayjs';
 import { Table } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
+import type { TableColumnsType } from 'antd';
 import { Select } from 'antd';
 import { Button } from 'antd';
 import type {
